fea50da762376b9ef5890921005c6a89d4548c43
Nmaa2 <141497711+Nmaa2@users.noreply.github.com>
2023-08-10 12:27:15 +0400

    problem of the day(1)

---
+++ session1-problem solving/script.js
@@ -0,0 +1,44 @@
+ 
+
+var n1;
+var n2;
+var n3;
+
+if (n1 > n2 && n1 >n3){
+    console.log("the first number is the greter = " + n1)
+}
+else if (n2 >n1 && n2 > n3){
+    console.log("the second number is the greter = " + n2)
+}
+else if (n3 >n1 && n3 > n2){
+    console.log("the third number is the greter = " + n3)
+}
+else {
+      console.log(" Thank ypu")
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+var use="AndAndr";
+var len=7;
+
+if (use.length == len){
+    console.log(" True")
+    console.log(use.pop())
+}
+else if (use.length != len){
+    console.log(" false")
+}
+else{
+    console.log(use.pop())
+}
